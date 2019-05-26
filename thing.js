5ac41df896eab4b728a9b06b1acf2726d22b7554
fastflowz <dakota.lewallen17@gmail.com>
2019-05-26 14:33:07 -0400

    working modal

--- thing.js
+++ thing.js
@@ -1,5 +1,22 @@
 const elem = document.getElementById('markdownInput')
+const bucketBtn = document.getElementById('bucketBtn')
+const modal = document.getElementById('myModal')
+const close = document.getElementsByClassName('close')[0]
 
 elem.addEventListener("keyup", () => {
     document.getElementById('markdownOutput').innerHTML = marked(elem.value)
 })
+
+bucketBtn.onclick = () => {
+    modal.style.display = "block"
+}
+
+close.onclick = () => {
+    modal.style.display = "none"
+}
+
+window.onclick = (event) => {
+    if(event.target == modal) {
+        modal.style.display = "none"
+    }
+}
